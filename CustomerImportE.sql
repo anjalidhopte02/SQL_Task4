90265434f812f27481e87ca41928ab049e1c0755
Anjali Dhopte <anjalidhopte02@gmail.com>
2024-07-30 09:01:06 +0530

    Add files via upload

---
+++ CustomerImportE.sql
@@ -0,0 +1,12 @@
+create table customer(
+	customer_id int8 primary key,
+    first_name varchar(50),
+    last_name varchar(50),
+	email varchar(100),
+    address_id int8
+
+
+)
+select * from customer
+
+copy
